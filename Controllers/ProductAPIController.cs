69fe29b98b49cc8e3e7df8172259ea017ef078f0
PraveenKhurana1986 <86887717+PraveenKhurana1986@users.noreply.github.com>
2021-07-19 01:38:35 +0530

    Add files via upload

---
+++ Controllers/ProductAPIController.cs
@@ -0,0 +1,58 @@
+﻿using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    public class ProductAPIController : ApiController
+    {
+        private readonly IProductRepository _iproductRepository = new ProductRepository();
+
+        [HttpGet]
+        [Route("api/Product/Get")]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _iproductRepository.GetProduct();
+        }
+
+        [HttpPost]
+        [Route("api/Product/Create")]
+        public async Task CreateAsync([FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _iproductRepository.Add(product);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/products/details/{id}")]
+        public async Task<Product> Details(int id)
+        {
+            var result = await _iproductRepository.GetProducts(id);
+            return result;
+        }
+
+        [HttpPut]
+        [Route("api/product/edit")]
+        public async Task EditAsync([FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _iproductRepository.Update(product);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/Product/Delete/{id}")]
+        public async Task DeleteConfirmedAsync(int id)
+        {
+            await _iproductRepository.Delete(id);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,203 @@
+﻿using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ShopBridge.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        readonly string apiBaseAddress = ConfigurationManager.AppSettings["apiBaseAddress"];
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Product> products = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseAddress);
+
+                var result = await client.GetAsync("product/get");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    products = await result.Content.ReadAsAsync<IList<Product>>();
+                }
+                else
+                {
+                    products = Enumerable.Empty<Product>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(products);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseAddress);
+
+                var result = await client.GetAsync($"products/details/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    product = await result.Content.ReadAsAsync<Product>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ProductName,ProductDescription,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(apiBaseAddress);
+
+                    var response = await client.PostAsJsonAsync("product/Create", product);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                    }
+                }
+            }
+            return View(product);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseAddress);
+
+                var result = await client.GetAsync($"products/details/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    product = await result.Content.ReadAsAsync<Product>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ProductId,ProductName,ProductDescription,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(apiBaseAddress);
+                    var response = await client.PutAsJsonAsync("product/edit", product);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseAddress);
+
+                var result = await client.GetAsync($"products/details/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    product = await result.Content.ReadAsAsync<Product>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiBaseAddress);
+
+                var response = await client.DeleteAsync($"Product/Delete/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+            }
+            return View();
+        }
+
+    }
+}
